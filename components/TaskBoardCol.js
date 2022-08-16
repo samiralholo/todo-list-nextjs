a098d4c802bf4b22f4da96e54a185837b4141cbe
Samir Alholo <samir.alholo@wizme.com>
2022-08-16 08:52:09 +0300

    Fetching Data inside Components

--- components/TaskBoardCol.js
+++ components/TaskBoardCol.js
@@ -1,4 +1,3 @@
-import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
@@ -6,10 +5,9 @@ import IconButton from "@mui/material/IconButton";
 import TaskBoardColStyles from "../styles/TaskBoardCol.module.css";
 import AddTaskIcon from "@mui/icons-material/AddTask";
 import { green } from "@mui/material/colors";
-import Icon from "@mui/material/Icon";
 import TaskItem from "./TaskItem";
 
-const TaskBoardCol = () => {
+const TaskBoardCol = ({ columnData, tasksList }) => {
   return (
     <div>
       <Card
@@ -18,30 +16,39 @@ const TaskBoardCol = () => {
       >
         <CardContent className={TaskBoardColStyles.tasks_container_box_content}>
           <div className={TaskBoardColStyles.colHeader}>
-            <div className={TaskBoardColStyles.badge_line}></div>
+            <div
+              className={TaskBoardColStyles.badge_line}
+              style={{ backgroundColor: columnData.columnColor }}
+            ></div>
             <div className={TaskBoardColStyles.colTitle}>
               <Typography variant="h5" component="div">
-                TO Do
+                {columnData.title}
               </Typography>
               <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                Things that need to be done.
+                {columnData.description}
               </Typography>
             </div>
-            <IconButton aria-label="settings">
-              <Icon sx={{ color: green[500], width: "100%", height: "100%" }}>
-                <AddTaskIcon sx={{ fontSize: 30 }} />
-              </Icon>
-            </IconButton>
+            {columnData.hasAddButton ? (
+              <IconButton
+                aria-label="settings"
+                className={TaskBoardColStyles.addButton}
+              >
+                <AddTaskIcon
+                  sx={{
+                    fontSize: 30,
+                    color: "white",
+                    backgroundColor: green[500],
+                    borderRadius: "200px",
+                  }}
+                />
+              </IconButton>
+            ) : null}
           </div>
 
           <div>
-            <TaskItem></TaskItem>
-            <TaskItem></TaskItem>
-            <TaskItem></TaskItem>
-            <TaskItem></TaskItem>
-            <TaskItem></TaskItem>
-            <TaskItem></TaskItem>
-            <TaskItem></TaskItem>
+            {tasksList?.map((task) => {
+              return <TaskItem key={task._id} taskData={task}></TaskItem>;
+            })}
           </div>
         </CardContent>
       </Card>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,76 @@
+import { useState } from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Button from "@mui/material/Button";
 import TaskBoardCol from "../components/TaskBoardCol";
+// import { v4 as uuid } from "uuid";
+import CircularProgress from "@mui/material/CircularProgress";
+import { useTodoListData } from "../hooks/useTodoListData";
+
+const BOARD_COLUMNS = {
+  ["todo"]: {
+    title: "TO Do",
+    status: "todo",
+    description: "Things that need to be done.",
+    hasAddButton: true,
+    columnColor: "#F66568",
+    items: [],
+  },
+  ["doing"]: {
+    title: "Doing",
+    status: "doing",
+    description: "What you're doing.",
+    hasAddButton: false,
+    columnColor: "#FFC773",
+    items: [],
+  },
+  ["done"]: {
+    title: "Done",
+    status: "done",
+    description: "Already done.",
+    hasAddButton: false,
+    columnColor: "#6BE795",
+    items: [],
+  },
+  ["archive"]: {
+    title: "Archive",
+    status: "archive",
+    description: "Not important but need to write down.",
+    hasAddButton: false,
+    columnColor: "#7389FF",
+    items: [],
+  },
+};
 
 export default function Home() {
+  const [columns, setColumns] = useState(BOARD_COLUMNS);
+
+  const onSuccess = (data) => {
+    console.log({ data });
+  };
+
+  const onError = (error) => {
+    console.log({ error });
+  };
+
+  const { isLoading, data, isError, error, refetch } = useTodoListData(
+    onSuccess,
+    onError
+  );
+
+  if (isLoading) {
+    return (
+      <div className={styles.spinnerContainerStyle}>
+        <CircularProgress size="20vh" />
+      </div>
+    );
+  }
+
+  const getTasksListByStatus = (status, taskList) => {
+    if (taskList) {
+      return taskList.filter((task) => task.status === status);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,10 +82,15 @@ export default function Home() {
         <h1 className={styles.title}>TODO List</h1>
 
         <div className={styles.boardColumns}>
-          <TaskBoardCol></TaskBoardCol>
-          <TaskBoardCol></TaskBoardCol>
-          <TaskBoardCol></TaskBoardCol>
-          <TaskBoardCol></TaskBoardCol>
+          {Object.entries(columns).map(([id, column], index) => {
+            return (
+              <TaskBoardCol
+                key={index}
+                columnData={column}
+                tasksList={getTasksListByStatus(column.status, data?.data)}
+              ></TaskBoardCol>
+            );
+          })}
         </div>
       </main>
     </div>
